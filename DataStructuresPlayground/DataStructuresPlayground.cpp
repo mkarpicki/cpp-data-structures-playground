9197637fe8b8b223012b19aa051eff433f228f51
mkarpicki <mkarpicki@gmail.com>
2014-12-25 23:30:30 +0100

    Made Stack working with any type delivered by customer). Unfortunately because of problems with Linker (since at least 1999 as I found in book) I hade to move whole code into header file (to define template only once). Seems that there are tools with better linkers but I see VS 2013 has still issues for now.. O_o

--- DataStructuresPlayground/DataStructuresPlayground.cpp
+++ DataStructuresPlayground/DataStructuresPlayground.cpp
@@ -2,12 +2,15 @@
 //
 
 #include "stdafx.h"
+#include "windows.h"
 #include "Stack.h"
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 
-	Stack * stack = new Stack();
+	Stack<int> * stack = new Stack<int>();
+	Stack<char> * stack2 = new Stack<char>();
 
 	stack->push(1);
 	stack->push(2);
@@ -17,6 +20,15 @@ int _tmain(int argc, _TCHAR* argv[])
 	stack->pop();
 	stack->pop();
 
+	stack2->push('a');
+	stack2->push('b');
+	stack2->push('c');
+
+	stack2->pop();
+	stack2->pop();
+	stack2->pop();
+
+	//system("PAUSE");
 
 	return 0;
 }
--- DataStructuresPlayground/Stack.h
+++ DataStructuresPlayground/Stack.h
@@ -1,29 +1,87 @@
 #pragma once
 
+template <class StackElement>
 class Stack
 {
 public:
 	Stack();
 	~Stack();
 
-	void push(int);
-	int pop();
+	void push(StackElement);
+	StackElement pop();
 
 private:
 
 	class StackItem {
 	public:
 		StackItem();
-		StackItem(int);
+		StackItem(StackElement);
 		~StackItem();
 
 		StackItem * previousItem;
-		int value;
+		StackElement value;
 	};
 
 	StackItem * lastItem;
 
+
 };
+
+template <class StackElement>
+Stack<StackElement>::Stack()
+{
+	lastItem = NULL;
+}
+
+template <class StackElement>
+Stack<StackElement>::~Stack()
+{
+}
+
+template <class StackElement>
+void Stack<StackElement>::push(StackElement value)
+{
+	StackItem * newItem = new StackItem(value);
+
+
+	if (lastItem != NULL)
+	{
+		newItem->previousItem = lastItem;
+	}
+
+	lastItem = newItem;
+
+	return;
+}
+
+template <class StackElement>
+StackElement Stack<StackElement>::pop()
+{
+	if (lastItem == NULL)
+	{
+		return NULL;
+	}
+
+	StackElement value = lastItem->value;
+	StackItem * itemToDrop = lastItem;
+
+	lastItem = lastItem->previousItem;
+
+	delete itemToDrop;
+
+	return value;
+}
+
+template <class StackElement>
+Stack<StackElement>::StackItem::StackItem(StackElement val)
+{
+	value = val;
+}
+
+template <class StackElement>
+Stack<StackElement>::StackItem::~StackItem()
+{
+}
 
 
 
